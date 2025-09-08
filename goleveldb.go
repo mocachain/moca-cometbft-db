bb2b7c87426cd7b7818f697c2f3cf25a97213bd7
athaswang1991 <athaswang1991@gmail.com>
2025-09-08 10:54:38 +0800

    feat: upgrade to cometbft-db v0.11.0

--- goleveldb.go
+++ goleveldb.go
@@ -21,7 +21,7 @@ func init() {
 	dbCreator := func(name string, dir string, opts ...*NewDatabaseOption) (DB, error) {
 		return NewGoLevelDB(name, dir, opts...)
 	}
-	registerDBCreator(GoLevelDBBackend, dbCreator, false)
+	registerDBCreator(GoLevelDBBackend, dbCreator)
 }
 
 type GoLevelDB struct {
@@ -58,11 +58,12 @@ func NewGoLevelDB(name string, dir string, opts ...*NewDatabaseOption) (*GoLevel
 }
 
 func NewGoLevelDBWithOpts(name string, dir string, o *opt.Options) (*GoLevelDB, error) {
-	dbPath := filepath.Join(dir, name+".db")
+	dbPath := filepath.Join(dir, name+".db") //nolint:goconst
 	db, err := leveldb.OpenFile(dbPath, o)
 	if err != nil {
 		return nil, err
 	}
+
 	database := &GoLevelDB{
 		db: db,
 	}
@@ -101,7 +102,8 @@ func (db *GoLevelDB) Set(key []byte, value []byte) error {
 	if value == nil {
 		return errValueNil
 	}
-	if err := db.db.Put(key, value, nil); err != nil {
+	err := db.db.Put(key, value, nil)
+	if err != nil {
 		return err
 	}
 	return nil
@@ -115,7 +117,9 @@ func (db *GoLevelDB) SetSync(key []byte, value []byte) error {
 	if value == nil {
 		return errValueNil
 	}
-	if err := db.db.Put(key, value, &opt.WriteOptions{Sync: true}); err != nil {
+
+	err := db.db.Put(key, value, &opt.WriteOptions{Sync: true})
+	if err != nil {
 		return err
 	}
 	return nil
@@ -126,7 +130,9 @@ func (db *GoLevelDB) Delete(key []byte) error {
 	if len(key) == 0 {
 		return errKeyEmpty
 	}
-	if err := db.db.Delete(key, nil); err != nil {
+
+	err := db.db.Delete(key, nil)
+	if err != nil {
 		return err
 	}
 	return nil
@@ -217,4 +223,9 @@ func (db *GoLevelDB) ReverseIterator(start, end []byte) (Iterator, error) {
 	}
 	itr := db.db.NewIterator(&util.Range{Start: start, Limit: end}, nil)
 	return newGoLevelDBIterator(itr, start, end, true), nil
+}
+
+// Compact range
+func (db *GoLevelDB) Compact(start, end []byte) error {
+	return db.db.CompactRange(util.Range{Start: start, Limit: end})
 }
